2c16fc6930a8315ac3b9fa7131d90df6ce1336e8
dosisod <39638017+dosisod@users.noreply.github.com>
2022-10-26 19:28:49 -0700

    Improve UX for creating new events

--- cal9000/render/event_manager.py
+++ cal9000/render/event_manager.py
@@ -19,6 +19,37 @@ def render_events(events: list[Event]) -> str:
     return f"Recurring events:\n\n{formatted}"
 
 
+EVENT_HELP_MSG = """
+Events are entered in one of the following forms:
+
+Monthly events:
+
+* N(th) of [every | the] month
+
+Weekly events:
+
+* every WEEKDAY
+* WEEKDAY
+"""
+
+
+def parse_event(description: str, event: str) -> Event | None:
+    match event.lower().split():
+        # TODO: move logic to the event itself
+        case [interval, "of", "every" | "the", "month"]:
+            return MonthlyEvent(description, interval_to_int(interval))
+
+        case ["every", day] | [day]:
+            try:
+                weekday = WEEK_DAY_NAMES.index(day.lower())
+
+                return WeeklyEvent(description, weekday)
+            except ValueError:
+                pass
+
+    return None
+
+
 def recurring_event_manager(db: DB, keyboard: Keyboard) -> View:
     while True:
         with ui_window():
@@ -31,22 +62,19 @@ def recurring_event_manager(db: DB, keyboard: Keyboard) -> View:
 
         if c == Keys.INSERT:
             with ui_window(full_reset=True):
-                event = input("> ")
+                print("Event description")
+                description = input("> ")
 
-            match event.lower().split():
-                # TODO: move logic to the event itself
-                case [interval, "of", "every" | "the", "month", "#", *rest]:
-                    comment = " ".join(rest)
-                    db.events.append(
-                        MonthlyEvent(comment, interval_to_int(interval))
-                    )
+                if not description:
+                    continue
 
-                case ["every", day, "#", *rest] | [day, "#", *rest]:
-                    comment = " ".join(rest)
+                print(EVENT_HELP_MSG)
 
-                    try:
-                        weekday = WEEK_DAY_NAMES.index(day.lower())
+                while True:
+                    format = input("> ")
+                    if not format:
+                        break
 
-                        db.events.append(WeeklyEvent(comment, weekday))
-                    except ValueError:
-                        pass
+                    if event := parse_event(description, format):
+                        db.events.append(event)
+                        break
--- test/render/test_event_manager.py
+++ test/render/test_event_manager.py
@@ -38,10 +38,11 @@ def test_quit() -> None:
 
 def test_insert_monthly_event() -> None:
     db = DB()
-    inserting = "1st of every month # comment"
 
     with disable_print():
-        with patch("builtins.input", lambda _: inserting):
+        with patch("builtins.input") as p:
+            p.side_effect = ["comment", "1st of every month"]
+
             kb = keyboard([Keys.INSERT, Keys.QUIT])
             states = list(recurring_event_manager(db, kb))
 
@@ -53,10 +54,11 @@ def test_insert_monthly_event() -> None:
 
 def test_insert_weekly_event() -> None:
     db = DB()
-    inserting = "every sunday # comment"
 
     with disable_print():
-        with patch("builtins.input", lambda _: inserting):
+        with patch("builtins.input") as p:
+            p.side_effect = ["comment", "every sunday"]
+
             kb = keyboard([Keys.INSERT, Keys.QUIT])
             states = list(recurring_event_manager(db, kb))
 
@@ -66,16 +68,51 @@ def test_insert_weekly_event() -> None:
     assert db.events == [WeeklyEvent("comment", weekday=0)]
 
 
-def test_invalid_weekly_event_doest_cause_error() -> None:
+def test_typing_invalid_event_will_ask_you_again() -> None:
     db = DB()
-    inserting = "every invalid_weekday # comment"
 
     with disable_print():
-        with patch("builtins.input", lambda _: inserting):
+        with patch("builtins.input") as p:
+            p.side_effect = [
+                "comment",
+                "every invalid_weekday",
+                "every monday",
+            ]
+
             kb = keyboard([Keys.INSERT, Keys.QUIT])
             states = list(recurring_event_manager(db, kb))
 
     assert len(states) == 2
-    assert "comment" not in states[1]
+    assert "comment" in states[1]
+
+    assert db.events == [WeeklyEvent(title="comment", weekday=1)]
+
+
+def test_empty_description_will_cause_early_exit() -> None:
+    db = DB()
+
+    with disable_print():
+        with patch("builtins.input") as p:
+            p.side_effect = [""]
+
+            kb = keyboard([Keys.INSERT, Keys.QUIT])
+            states = list(recurring_event_manager(db, kb))
+
+    assert len(states) == 2
+
+    assert not db.events
+
+
+def test_empty_event_format_will_cause_early_exit() -> None:
+    db = DB()
+
+    with disable_print():
+        with patch("builtins.input") as p:
+            p.side_effect = ["comment", ""]
+
+            kb = keyboard([Keys.INSERT, Keys.QUIT])
+            states = list(recurring_event_manager(db, kb))
+
+    assert len(states) == 2
 
     assert not db.events
